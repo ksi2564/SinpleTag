0c8e1e0425ebfdbbbc848f1aa0f2722c42305d34
Inys <kimsungin5@gmail.com>
2022-08-05 17:43:33 +0900

    테코레이터 파일 커밋

---
+++ accountapp/decorators.py
@@ -0,0 +1,15 @@
+from django.contrib import messages
+from django.shortcuts import redirect
+
+
+def is_login(func):
+    def decorated(request, *args, **kwargs):
+        session = request.user.pk
+
+        if session:
+            return func(request, *args, **kwargs)
+        else:
+            messages.error(request, '로그인 안하면 큰일나!', extra_tags='danger')
+            return redirect('/')
+
+    return decorated
