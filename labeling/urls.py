5e1c0a925d11ffd03e17f72f7562417ea6d261c9
Inys <kimsungin5@gmail.com>
2022-08-08 12:15:41 +0900

    라벨링 디테일 하드코딩 제거(카테고리는 하드코딩) 분류 검수 리스트 페이지에 데이터셋 다운 버튼 생성

--- labeling/urls.py
+++ labeling/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from labeling.views import label_detail, status_board, inspect_list, inspect_detail, LabelingList, LabelingLoadImage
+from labeling.views import status_board, inspect_list, inspect_detail, LabelingList, LabelingLoadImage, LabelingDetail
 
 app_name = 'labeling'
 
 urlpatterns = [
     path('', LabelingList.as_view(), name='label_list'),
-    path('image_num/', label_detail, name='label_detail'),
+    path('<int:pk>/', LabelingDetail.as_view(), name='label_detail'),
     path('inspect/', inspect_list, name='inspect_list'),
     path('inspect/image_num/', inspect_detail, name='inspect_detail'),
     path('board/', status_board, name='status_board'),
