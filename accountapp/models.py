d4a3d9cbad2def1b4a11f678d65eecd79b781ad9
ksi2564 <kimsungin5@gmail.com>
2022-09-19 11:31:33 +0900

    소셜 로그인 외 기본 로그인 기능 구현 및 개인 작업량 체크 기능

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,8 +1,62 @@
+# from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
+#
+# class UserManager(BaseUserManager):
+#     def create_user(self, email, name, password=None):
+#         if not email:
+#             raise ValueError('이메일을 입력해주세요')
+#
+#         user = self.model(
+#             email=self.normalize_email(email),
+#             name=name,
+#         )
+#
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+#
+#     def create_superuser(self, email, name, password):
+#         user = self.create_user(
+#             email,
+#             password=password,
+#             name=name,
+#         )
+#         user.is_admin = True
+#         user.save(using=self._db)
+#         return user
+#
+#
+# class User(AbstractBaseUser):
+#     email = models.EmailField(
+#         verbose_name='email',
+#         max_length=255,
+#         unique=True,
+#     )
+#     name = models.CharField(max_length=128)
+#     is_active = models.BooleanField(default=True)
+#     is_admin = models.BooleanField(default=False)
+#
+#     objects = UserManager()
+#
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['name']
+#
+#     def __str__(self):
+#         return self.email
+#
+#     def has_perm(self, perm, obj=None):
+#         return True
+#
+#     def has_module_perms(self, app_label):
+#         return True
+#
+#     @property
+#     def is_staff(self):
+#         return self.is_admin
 
 class RequestPermission(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
